d887250d22cc71bd27cca361c3e36f34abb2048e
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-02-01 17:40:25 -0300

    Estrutura da tela de Login pronta

--- lib/src/modules/auth/login/login_page.dart
+++ lib/src/modules/auth/login/login_page.dart
@@ -5,6 +5,69 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    
+    final sizeOf = MediaQuery.sizeOf(context);
+    
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints(
+            minHeight: sizeOf.height,
+          ),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/background_login.png"),
+              fit: BoxFit.cover,
+            )
+          ),
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.all(40),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16)
+              ),
+              constraints: BoxConstraints(
+                maxWidth: sizeOf.width * 0.8,
+              ),
+              child: Column(
+                children: [
+                  const Text(
+                    "Login",
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      label: Text("Email"),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      label: Text("Senha"),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  SizedBox(
+                    width: sizeOf.width * 0.8,
+                    height: 48,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("ENTRAR"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
