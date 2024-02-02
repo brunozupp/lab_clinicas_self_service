f75d1df88fa6b47e8b635564ece9193eb1120e41
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-02-02 17:30:09 -0300

    Criado lógica de redirecionamento

--- lib/src/modules/self_service/self_service_module.dart
+++ lib/src/modules/self_service/self_service_module.dart
@@ -3,6 +3,7 @@ import 'package:flutter_getit/flutter_getit.dart';
 import 'package:lab_clinicas_self_service/src/modules/self_service/done/done_page.dart';
 import 'package:lab_clinicas_self_service/src/modules/self_service/find_patient/find_patient_page.dart';
 import 'package:lab_clinicas_self_service/src/modules/self_service/patient/patient_page.dart';
+import 'package:lab_clinicas_self_service/src/modules/self_service/self_service_controller.dart';
 import 'package:lab_clinicas_self_service/src/modules/self_service/self_service_page.dart';
 import 'package:lab_clinicas_self_service/src/modules/self_service/who_i_am/who_i_am_page.dart';
 
@@ -11,6 +12,11 @@ import 'documents/scan/documents_scan_page.dart';
 import 'documents/scan_confirm/documents_scan_confirm_page.dart';
 
 class SelfServiceModule extends FlutterGetItModule {
+
+  @override
+  List<Bind<Object>> get bindings => [
+    Bind.lazySingleton((i) => SelfServiceController()),
+  ];
   
   @override
   String get moduleRouteName => "/self-service";
