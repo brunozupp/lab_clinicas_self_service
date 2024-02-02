df50829994d355b44b5e5deac028a7e32772ba63
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-02-02 13:18:11 -0300

    Criado módulo de Home - sem layout

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_getit/flutter_getit.dart';
 import 'package:lab_clinicas_core/lab_clinicas_core.dart';
 import 'package:lab_clinicas_self_service/src/binding/lab_clinicias_application_binding.dart';
 import 'package:lab_clinicas_self_service/src/modules/auth/auth_module.dart';
+import 'package:lab_clinicas_self_service/src/modules/home/home_module.dart';
 
 import 'src/pages/splash/splash_page.dart';
 
@@ -37,6 +38,7 @@ class LabClinicasSelfServiceApp extends StatelessWidget {
       ],
       modules: [
         AuthModule(),
+        HomeModule(),
       ],
     );
   }
